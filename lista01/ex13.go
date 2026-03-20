b7f50a4240535878ea760c6e5af6f17304bfd1bb
João vitor <156925293+joaovitor0630@users.noreply.github.com>
2026-03-20 09:49:33 -0300

    Add grading system based on score input

---
+++ lista01/ex13.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nota float64
+	fmt.Scan(&nota)
+
+	var conceito string
+	if nota >= 9.0 {
+		conceito = "A"
+	} else if nota >= 7.5 {
+		conceito = "B"
+	} else if nota >= 6.0 {
+		conceito = "C"
+	} else {
+		conceito = "D"
+	}
+
+	fmt.Printf("NOTA = %.1f CONCEITO = %s\n", nota, conceito)
+}
