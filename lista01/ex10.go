788440f59611f60702eb7184b902c832027d194b
João vitor <156925293+joaovitor0630@users.noreply.github.com>
2026-03-20 09:48:40 -0300

    Add program to calculate determinant of a matrix

---
+++ lista01/ex10.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, d float64
+	fmt.Scan(&a, &b, &c, &d)
+
+	det := (a * d) - (b * c)
+	fmt.Printf("O VALOR DO DETERMINANTE E = %.2f\n", det)
+}
